896ab40de5f67f3ab37dc570c2e642769633d70e
Luiz Henrique <luizboaventura24@gmail.com>
2023-08-21 19:47:19 -0300

    A

---
+++ src/components/Chart/MonthlyIncome.vue/MonthlyIncomeRoot.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-chart w-100">
+    
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+$main-background: #17191c;
+$secondary-color: #25272c;
+
+.container-chart {
+  background-color: $secondary-color;
+}
+</style>
--- src/views/master/VisaoGeralView.vue
+++ src/views/master/VisaoGeralView.vue
@@ -1,27 +1,23 @@
 <template>
   <BalancePanel />
-  <!--<div class="container-chart ma-3">
+  <div class="container-chart ma-3">
     <v-row>
       <v-col class="d-flex justify-center" cols="12" md="6">
-        <ChartMSC />
+        <MonthlyIncomeRoot />
       </v-col>
       <v-col class="d-flex justify-center" cols="12" md="6">
 
       </v-col>
     </v-row>
-
-  </div>-->
-  <div class="text-center py-12 font-italic text-grey-lighten-1">
-    <p>Sistema de gráficos em desenvolvimento...</p>
   </div>
 </template>
 
 <script setup>
 import BalancePanel from '@/components/BalanceContainer/BalancePanel.vue';
-//import ChartMSC from '@/components/Chart-MSC-V1/ChartMSC.vue';
+import MonthlyIncomeRoot from '@/components/Chart/MonthlyIncome/MonthlyIncomeRoot.vue';
+
 </script>
 
 <style scoped lang="scss">
-
 
 </style>
