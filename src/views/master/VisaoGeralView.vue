bbb5c6ec3f463e3cc80d9bd089c4fc0735c45c36
Luiz Henrique <luizboaventura24@gmail.com>
2023-08-22 14:49:22 -0300

    Grafico adicionado

--- src/views/master/VisaoGeralView.vue
+++ src/views/master/VisaoGeralView.vue
@@ -6,7 +6,7 @@
         <MonthlyIncomeRoot />
       </v-col>
       <v-col class="d-flex justify-center" cols="12" md="6">
-
+        <MonthlyExpenseRoot />
       </v-col>
     </v-row>
   </div>
@@ -15,6 +15,7 @@
 <script setup>
 import BalancePanel from '@/components/BalanceContainer/BalancePanel.vue';
 import MonthlyIncomeRoot from '@/components/Chart/MonthlyIncome/MonthlyIncomeRoot.vue';
+import MonthlyExpenseRoot from '@/components/Chart/MonthlyIncome/MonthlyExpenseRoot.vue';
 
 </script>
 
