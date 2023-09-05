1ade0c6d66d1fab2503206048ef6fbd0d179db68
Luiz Henrique <luizboaventura24@gmail.com>
2023-09-05 19:28:43 -0300

    A

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import FirebaseApp from 'firebase/app'
+import FirebaseApp from "firebase/app"
  
 loadFonts()
 
